64573f3a040aa3a43cf0c0c2a582823c6c36f5b7
0-1C-H <0.1cheng.h@gmail.com>
2017-02-18 21:51:11 +0800

    Monky problem (re-upload)

--- Joseph problem.c
+++ Joseph problem.c
@@ -6,27 +6,27 @@ int main(void)
 	int M;
 	int K;
 	int monky[1001];
-	printf("input M\n");
+	printf("输入M\n");
 	scanf("%d", &M);
-	printf("input K\n");
+	printf("输入K\n");
 	scanf("%d", &K);
 	int restnum = M;
-	int ind0=0;//init all to 0
+	int ind0=0;//全部初始化为0
 	for (ind0 = 0; ind0 <= 1001; ind0++)
 	{
 		monky[ind0] = 0;
 	}
 	
-	int ind1=1;//inti other M to 1 ecxcpt the first one  
+	int ind1=1;//空出第一个并把M个格子初始化为1 
 	while (ind1 <= M)
 	{
 		monky[ind1] = 1;
 		ind1++;
 	}
 	
-	int ind2;//scanner
-	int cou=0;//kick out 1 in every K
-	int ind3;//output scanner
+	int ind2;//扫描器
+	int cou=0;//K个出1个
+	int ind3;//输出扫描器
 	int tim = 1;
 
 L:	ind2 = 0;
